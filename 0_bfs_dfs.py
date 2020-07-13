799b86022cf952f2bae89c1de5f1fb8be02cc958
Jerome Yu <jerome1993@qq.com>
2020-07-13 01:57:03 -0400

    bfs dfs

--- 0_bfs_dfs.py
+++ 0_bfs_dfs.py
@@ -19,7 +19,19 @@ def bfs(graph, root):
                 visited.add(neighbour)
                 queue.append(neighbour)
 
+def bfs_paths(graph, start, goal):
+    res_path = []
+    queue = [(start, [start])]
+    while queue:
+        (vertex, path) = queue.pop(0)
+        for next in set(graph[vertex]) - set(path):
+            if next == goal:
+                res_path.append(path + [next])
+            else:
+                queue.append((next, path + [next]))
+    return(res_path)
 
+    
 # dfs
 
 def dfs(graph, start):
@@ -35,3 +47,16 @@ def dfs(graph, start):
             visited.add(vertex)
             stack.extend(set(graph[vertex]) - visited)
     return visited
+
+
+def dfs_paths(graph, start, goal):
+    path_res = []
+    stack = [(start, [start])]
+    while stack:
+        (vertex, path) = stack.pop()
+        for next in set(graph[vertex]) - set(path):
+            if next == goal:
+                path_res.append(path + [next])
+            else:
+                stack.append((next, path + [next]))
+    return(path_res)  
